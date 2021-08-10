3532ec889af672d79144f69a28f485c152d00d18
Khaled Ben <khaled.benmerabet@gmail.com>
2021-08-10 05:22:26 +0200

    Update Readme and userdata scripts

--- DET-AWS-011/user_data/user_data_amazonlinux2.sh
+++ DET-AWS-011/user_data/user_data_amazonlinux2.sh
@@ -6,4 +6,8 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
   service httpd start
   chkconfig httpd on
   chkconfig --list httpd
+  mkdir /var/www
+  mkdir /var/www/html
+  cd /var/www/html
+  git clone https://github.com/digininja/DVWA.git
   echo "Hello from user-data!"
