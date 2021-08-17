f5b242525b621a4737aaa52db3a51d99b6d9b93a
Khaled Ben <khaled.benmerabet@gmail.com>
2021-08-17 03:33:07 +0200

    Refactoring different cdk apps in one cdk app with multi stacks

--- AWS-LABS/app.py
+++ AWS-LABS/app.py
@@ -9,11 +9,13 @@ from aws_cdk import core as cdk
 # being updated to use `cdk`.  You may delete this import if you don't need it.
 from aws_cdk import core
 
+# from aws_labs.aws_labs_stack import AwsLabsStack
+from aws_labs_core.aws_labs_core_stack import AwsLabsCoreStack
 from det_aws_011.det_aws_011_stack import DetAws011Stack
 
 
 app = core.App()
-DetAws011Stack(app, "DetAws011Stack",
+# AwsLabsStack(app, "AwsLabsStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
@@ -21,7 +23,7 @@ DetAws011Stack(app, "DetAws011Stack",
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
 
-    env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+    #env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
@@ -29,7 +31,9 @@ DetAws011Stack(app, "DetAws011Stack",
     #env=core.Environment(account='123456789012', region='us-east-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
-# TestStack(app, "TestStack",env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')) )
+    # )
+
+labs_core = AwsLabsCoreStack(app, "AwsLabsCoreStack")
+det_aws_011 = DetAws011Stack(app, "DetAws011Stack", core_vpc=labs_core.vpc)
 
 app.synth()
--- AWS-LABS/setup.py
+++ AWS-LABS/setup.py
@@ -6,22 +6,27 @@ with open("README.md") as fp:
 
 
 setuptools.setup(
-    name="aws_labs_core",
+    name="aws_labs",
     version="0.0.1",
 
-    description="An empty CDK Python app",
+    description="KHLABS : AWS LABS CDK APP",
     long_description=long_description,
     long_description_content_type="text/markdown",
 
-    author="author",
+    author="Kh B.",
 
-    package_dir={"": "aws_labs_core"},
-    packages=setuptools.find_packages(where="aws_labs_core"),
+    package_dir={"": "aws_labs"},
+    packages=setuptools.find_packages(where="aws_labs"),
 
     install_requires=[
         "aws-cdk.core==1.116.0",
-        "aws-cdk.aws-s3",
-        "aws-cdk.aws-ec2"
+        "aws_cdk.aws_ec2==1.116.0",
+        "aws_cdk.aws_elasticloadbalancingv2==1.116.0",
+        "aws_cdk.aws_autoscaling==1.116.0",
+        "aws_cdk.aws_lambda==1.116.0",
+        "aws_cdk.aws_secretsmanager==1.116.0",
+        "aws-cdk.aws_wafv2==1.116.0",
+        "aws-cdk.aws-s3==1.116.0"
     ],
 
     python_requires=">=3.6",
